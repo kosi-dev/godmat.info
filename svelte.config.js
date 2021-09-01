8c62346b9526f6766340192bdc05e7534759609e
magnetenstad <tenstad.magne@gmail.com>
2021-09-01 12:16:36 +0200

    Add adapter-static and GitHub build action

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,18 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		// Comment the paths if wants to run in dev mode.
+		paths: {
+			base: '/groceries2',
+			assets: '/groceries2'
+		}
 	}
 };
 
