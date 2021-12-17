de251e0a8bac64a76573c2c0e19a2ec7c8f7495c
magnetenstad <tenstad.magne@gmail.com>
2021-12-17 02:13:30 +0100

    Deploy to firebase

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+let ssrServerServer;
+exports.ssrServer = functions.https.onRequest(async (request, response) => {
+	if (!ssrServerServer) {
+			functions.logger.info("Initialising SvelteKit SSR Handler");
+			ssrServerServer = require("./ssrServer/index").default;
+			functions.logger.info("SvelteKit SSR Handler initialised!");
+	}
+	functions.logger.info("Requested resource: " + request.originalUrl);
+	return ssrServerServer(request, response);
+});
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import firebase from "svelte-adapter-firebase";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,12 +10,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: firebase(),
 	}
 };
 
