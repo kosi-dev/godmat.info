2e03f8cad9be8f46a8abcb205c12e35da32c124c
magnetenstad <tenstad.magne@gmail.com>
2021-12-22 00:47:28 +0100

    Change chart lib

--- src/lib/ui/NutritionDiagram.svelte
+++ src/lib/ui/NutritionDiagram.svelte
@@ -1,91 +1,39 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Recommended from './recommended.json';
+  	import Chart from 'svelte-frappe-charts';
 
 	export let nutrition: object;
 
-	let donutChart;
 	let selectedGender = 'Male';
 	let selectedAge = '18-30 y';
-	let barChartVitamins;
-	let barChartMinerals;
-
-	function createDonutChart(keys, data: Array<number> = null) {
-		if (data == null) {
-			data = [];
-			for (let key of keys) {
-				data.push(nutrition[key]);
-			}
-		}
-		return {
-			chart: {
-				type: 'donut',
-				height: '350'
-			},
-			series: data,
-			labels: keys
-		};
-	}
-
-	const chart = (node, options) => {
-		let myChart = new ApexCharts(node, options);
-		myChart.render();
-
-		return {
-			update(options) {
-				myChart.updateOptions(options);
-			},
-			destroy() {
-				myChart.destroy();
-			}
-		};
+	let donutData = {
+		labels: ['Karbohydrat', 'Protein', 'Fett'],
+		datasets: [{ values: [
+			nutrition['Karbo'] * 4,
+			nutrition['Protein'] * 4,
+			nutrition['Fett'] * 9
+		] }]
 	};
+	$: vitaminsData = customData(
+		['Vit A', 'Vit D', 'Vit E', 'Vit B1', 'Vit B2', 'Vit B3', 'Vit B6', 'Folat', 'Vit C'],
+		selectedGender,
+		selectedAge
+	);
+	$: mineralsData = customData(
+		['Ca', 'P', 'K', 'Mg', 'Fe', 'Zn', 'Cu', 'I', 'Se'],
+		selectedGender,
+		selectedAge
+	);
 
-	onMount(async () => {
-		let data = [];
-		data.push(nutrition['Karbo'] * 4);
-		data.push(nutrition['Protein'] * 4);
-		data.push(nutrition['Fett'] * 9);
-		donutChart = createDonutChart(['Karbohydrat', 'Protein', 'Fett'], data);
-		rebuildCustomBarCharts();
-	});
-
-	function rebuildCustomBarCharts() {
-		barChartVitamins = customBarChart(
-			['Vit A', 'Vit D', 'Vit E', 'Vit B1', 'Vit B2', 'Vit B3', 'Vit B6', 'Folat', 'Vit C'],
-			selectedGender,
-			selectedAge
-		);
-		barChartMinerals = customBarChart(
-			['Ca', 'P', 'K', 'Mg', 'Fe', 'Zn', 'Cu', 'I', 'Se'],
-			selectedGender,
-			selectedAge
-		);
-	}
-
-	function customBarChart(keys, selectedGender, selectedAge) {
+	function customData(keys, selectedGender, selectedAge) {
 		let data = [];
 		for (let key of keys) {
 			data.push((nutrition[key] / Recommended[key][selectedGender][selectedAge]).toFixed(2));
 		}
 		return {
-			chart: {
-				type: 'bar',
-				height: '350'
-			},
-			series: [
-				{
-					name: 'Percentage of daily recommendayion',
-					data: data
-				}
-			],
-			xaxis: {
-				categories: keys
-			},
-			yaxis: {
-				min: 0.0,
-				max: 1.0
-			}
+			labels: keys,
+			datasets: [{ values: data }]
 		};
 	}
 </script>
@@ -96,14 +44,14 @@
 	<h3>Energi</h3>
 
 	<p>{nutrition['Energi2']} kcal, med følgende fordeling:</p>
-	{#if donutChart}
-		<div use:chart={donutChart} />
+	{#if donutData}
+		<Chart data={donutData} type="donut" />
 	{/if}
 
 	<h3>Vitaminer og mineraler</h3>
 	<p>
 		Prosent av anbefalt daglig inntak for
-		<select bind:value={selectedGender} on:change={rebuildCustomBarCharts}>
+		<select bind:value={selectedGender}>
 			{#each ['Male', 'Female'] as gender}
 				<option value={gender}>
 					{gender}
@@ -111,7 +59,7 @@
 			{/each}
 		</select>
 
-		<select bind:value={selectedAge} on:change={rebuildCustomBarCharts}>
+		<select bind:value={selectedAge}>
 			{#each ['6-11 m', '12-23 m', '2-5 y', '6-9 y', '10-13 y', '14-17 y', '18-30 y', '31-60 y', '61-74 y', '>75 y'] as age}
 				<option value={age}>
 					{age}
@@ -120,10 +68,10 @@
 		</select>
 	</p>
 
-	{#if barChartVitamins}
-		<div use:chart={barChartVitamins} />
+	{#if vitaminsData}
+		<Chart data={vitaminsData} type="bar" />
 	{/if}
-	{#if barChartMinerals}
-		<div use:chart={barChartMinerals} />
+	{#if mineralsData}
+		<Chart data={mineralsData} type="bar" />
 	{/if}
 {/if}
